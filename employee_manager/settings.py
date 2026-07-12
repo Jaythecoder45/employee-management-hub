b02ef31fb34bd453dcd1f5db96a49fd4f9c991f6
Jaythecoder45 <jaydeepshinde9391js@gmail.com>
2026-07-13 01:14:08 +0530

    Update database configuration fallback checks

--- employee_manager/settings.py
+++ employee_manager/settings.py
@@ -92,12 +92,18 @@ WSGI_APPLICATION = 'employee_manager.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
-}
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
